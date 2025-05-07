0504a2a6e6127b9ff53d93fdb232e2e003a75020
sachin barali <baralisachin472@gmail.com>
2025-05-07 23:29:32 +0545

    feat:Content Model,Item model added, custom Orderfield added

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -32,6 +35,55 @@ class Module(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
     title = models.CharField(max_length=200)
     description = models.TextField(blank = True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    def __str__(self):
+        return f'{self.order}. {self.title}'
+
+    class Meta:
+        ordering = ['order']
+    
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='contents')
+    content_type = models.ForeignKey(
+        ContentType, 
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in': ('text', 'file', 'image', 'video')
+        }
+    )
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type','object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
+
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_related')
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.title
+    
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.ImageField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
